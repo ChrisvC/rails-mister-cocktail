8cd28130bea65782985bceb669743bbf741bdd36
Christophe van Cutsem <christophevc91@gmail.com>
2018-03-15 12:27:38 +0100

    first model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create, :delete, :destroy] do
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+  root "cocktails#index"
+  resources :reviews, only: [:destroy]
+  resources :doses, only: [:destroy]
 end
