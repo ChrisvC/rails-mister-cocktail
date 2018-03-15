3a8c7b2f76fe7491d91fa80d6b2e112154c71d2f
Christophe van Cutsem <christophevc91@gmail.com>
2018-03-15 12:36:18 +0100

    review model and completion of models' content

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_namy :doses
+  has_many :ingredients, through: :doses
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315110939) do
+ActiveRecord::Schema.define(version: 20180315113537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20180315110939) do
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.string "doses"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
